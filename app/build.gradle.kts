f495f7cee53aa448e78b931027b5748c7a1fbd5a
Matthew Berthoud <mwberthoud@wm.edu>
2024-02-19 08:50:02 -0500

    Text fields and button, setting up database test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services") // firebase
 }
 
 android {
@@ -44,4 +45,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2")) // firebase
+    // TODO: Add the dependencies for Firebase products we want to use
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }

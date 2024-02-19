eab22873f8819609736b34c4a0855534430257ea
Matthew Berthoud <mwberthoud@wm.edu>
2024-02-19 10:28:57 -0500

    Quote data saving to database

--- app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.mariokarttournamentorganizer;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String QUOTE_KEY = "quote";
+    public static final String AUTHOR_KEY = "author";
+    public static final String TAG = "InspiringQuote";
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void saveQuote(View view) {
+        EditText quoteView = (EditText) findViewById(R.id.editTextQuote);
+        EditText authorView = (EditText) findViewById(R.id.editTextAuthor);
+        String quoteText = quoteView.getText().toString();
+        String authorText = authorView.getText().toString();
+
+        if (quoteText.isEmpty() || authorText.isEmpty()) {
+            return;
+        }
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put(QUOTE_KEY, quoteText);
+        dataToSave.put(AUTHOR_KEY, authorText);
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d(TAG, "Document has been saved!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Document was not saved!");
+            }
+        });
+
+        Log.d("MainActivity", "Quote saved");
     }
 }

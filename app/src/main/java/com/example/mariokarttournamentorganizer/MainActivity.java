d62aafa7209b660b6aea4b2acf5264ffdb8f6fe8
Matthew Berthoud <mwberthoud@wm.edu>
2024-02-27 16:28:25 -0500

    put in some message composing code, and intent-filter, nothing called yet

--- app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mariokarttournamentorganizer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void composeMmsMessage(String message, Uri attachment) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }

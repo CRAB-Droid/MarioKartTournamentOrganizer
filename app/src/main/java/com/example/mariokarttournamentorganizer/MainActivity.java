10de1320ac1746ac79b20e01391191bbf2054ea5
Matthew Berthoud <mwberthoud@wm.edu>
2024-02-27 18:07:54 -0500

    tried to do message with photo, can't get it. Going to try text with no photo

--- app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.mariokarttournamentorganizer;
 
+import static android.net.Uri.fromFile;
+
+import static java.net.Proxy.Type.HTTP;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void composeMmsMessage(String message, Uri attachment) {
+    public void processClick(View view) {
+        EditText messageView = (EditText) findViewById(R.id.messageTextInput);
+        String messageText = messageView.getText().toString();
+
+        if (messageText.isEmpty()) {
+            return;
+        }
+
+        System.out.println(messageText);
+        Uri messageImg = fromFile(new File("/res/raw/testimage.png"));
+            // works even with bad pathnames
+        if (messageImg == null) {
+            System.out.println("image null");
+        }
+        composeMmsMessage(messageText, messageImg);
+
+    }
+    public void composeMmsMessage(String message, Uri attachment){
+        System.out.println("composing");
         Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setType("text/plain");
+        intent.setData(Uri.parse("smsto:"));  // Only SMS apps respond to this.
         intent.putExtra("sms_body", message);
         intent.putExtra(Intent.EXTRA_STREAM, attachment);
         if (intent.resolveActivity(getPackageManager()) != null) {
+            System.out.println("starting");
             startActivity(intent);
+        } else {
+            System.out.println("something wrong");
+                // always goes here, never works
         }
     }
 }

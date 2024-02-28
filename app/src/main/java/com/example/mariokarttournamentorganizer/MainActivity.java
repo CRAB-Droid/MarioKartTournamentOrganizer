091693434322c06e9ae8dfd24dbf659b4577b25c
Matthew Berthoud <mwberthoud@wm.edu>
2024-02-28 16:53:35 -0500

    sms no text. Maybe it works? can't test from device with no SIM card

--- app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/MainActivity.java
@@ -5,12 +5,17 @@ import static android.net.Uri.fromFile;
 import static java.net.Proxy.Type.HTTP;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 
@@ -20,37 +25,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendButton = (Button) findViewById(R.id.sendMessageButton);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.SEND_SMS}, 100);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED)
+                    sendSMS();
+                else {
+                    System.out.println("not permitted");
+
+                }
+            }
+        });
     }
 
-    public void processClick(View view) {
-        EditText messageView = (EditText) findViewById(R.id.messageTextInput);
-        String messageText = messageView.getText().toString();
+    public void sendSMS() {
+        String message = ((EditText)findViewById(R.id.messageTextInput)).getText().toString();
+        String number = ((EditText)findViewById(R.id.phoneNumberInput)).getText().toString();
 
-        if (messageText.isEmpty()) {
+        System.out.println(number + ":  " + message);
+
+        if (message.isEmpty() || number.isEmpty()) {
             return;
         }
 
-        System.out.println(messageText);
-        Uri messageImg = fromFile(new File("/res/raw/testimage.png"));
-            // works even with bad pathnames
-        if (messageImg == null) {
-            System.out.println("image null");
-        }
-        composeMmsMessage(messageText, messageImg);
-
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, message, null, null);
     }
-    public void composeMmsMessage(String message, Uri attachment){
-        System.out.println("composing");
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("smsto:"));  // Only SMS apps respond to this.
-        intent.putExtra("sms_body", message);
-        intent.putExtra(Intent.EXTRA_STREAM, attachment);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            System.out.println("starting");
-            startActivity(intent);
-        } else {
-            System.out.println("something wrong");
-                // always goes here, never works
-        }
-    }
+//    public void composeMmsMessage(String message, Uri attachment){
+//        System.out.println("composing");
+//        Intent intent = new Intent(Intent.ACTION_SENDTO);
+//        intent.setData(Uri.parse("smsto:"));  // Only SMS apps respond to this.
+//        intent.putExtra("sms_body", message);
+//        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            System.out.println("starting");
+//            startActivity(intent);
+//        } else {
+//            System.out.println("something wrong");
+//                // always goes here, never works
+//        }
+//    }
 }

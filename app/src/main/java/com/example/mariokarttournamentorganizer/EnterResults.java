5a9fbca7b929a19bc7d2308638742b8242dc89e4
Ada Rigsby <acrigsby@email.wm.edu>
2024-03-01 16:11:56 -0500

    added more to enter results class

--- app/src/main/java/com/example/mariokarttournamentorganizer/EnterResults.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/EnterResults.java
@@ -1,15 +1,23 @@
 package com.example.mariokarttournamentorganizer;
 
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class EnterResults extends AppCompatActivity {
 
     private EditText results;
+
+    private Button submit;
+
+    //private Button photo;
+
+    private String resultsStr;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,5 +25,24 @@ public class EnterResults extends AppCompatActivity {
         setContentView(R.layout.enter_results);
 
         results = (EditText) findViewById(R.id.resultsInputEditTextNumber);
+
+        //Set up submit button
+        submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(v ->
+        {
+            resultsStr = results.getText().toString();
+            if (!resultsStr.isEmpty()) {
+                //TODO Setup button to store results into database
+            } else {
+                Toast.makeText(EnterResults.this,
+                        "Please fill out necessary field.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        photo = (Button) findViewById(R.id.photoButton);
+//        photo.setOnClickListener(v->
+//        {
+//        });
     }
 }
+

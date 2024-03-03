a3ce7f53e3dac40c4b0d3d95e5c822042e3183df
Ada Rigsby <acrigsby@email.wm.edu>
2024-03-03 16:58:07 -0500

    starting working on writing to firebase from create ACT and enter results

--- app/src/main/java/com/example/mariokarttournamentorganizer/CreateACTActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/CreateACTActivity.java
@@ -2,11 +2,21 @@ package com.example.mariokarttournamentorganizer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateACTActivity extends AppCompatActivity {
     private Button createButton;
@@ -16,6 +26,21 @@ public class CreateACTActivity extends AppCompatActivity {
     private String timeStr;
     private EditText location;
     private String locationStr;
+
+    //for firebase
+    public static final String ID_FIELD = "ID";
+    public static final String ADMIN_FIELD = "adminID";
+    public static final String DATE_FIELD = "date";
+    public static final String TIME_FIELD = "time";
+    public static final String LOCATION_FIELD = "location";
+    public static final String PLAYERS_FIELD = "players";
+    public static final String TAG = "creatingACT";
+    public static final String COMPLETED_FIELD = "completed";
+    public static final String RESULT_FIELD = "result";
+
+    private FirebaseFirestore mDocRef = FirebaseFirestore.getInstance();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +50,12 @@ public class CreateACTActivity extends AppCompatActivity {
         date = (EditText) findViewById(R.id.editTextDate);
         time = (EditText) findViewById(R.id.editTextTime);
         location = (EditText) findViewById(R.id.editTextLocation);
+
+        //finding which act will be created
+//        boolean notFound = true;
+//        for (int i = 1; notFound; i++){
+//
+//        }
 
 
         //Set up create button
@@ -36,6 +67,33 @@ public class CreateACTActivity extends AppCompatActivity {
             locationStr = location.getText().toString();
             if(!dateStr.isEmpty() && !timeStr.isEmpty() && !locationStr.isEmpty()) {
                 //TODO Setup button to store input activity into database
+
+                //creating a new document in firebase
+                Map<String, Object> act1 = new HashMap<>();
+                act1.put(ID_FIELD, 1);
+                act1.put(ADMIN_FIELD, 3);
+                act1.put(COMPLETED_FIELD, false); //default
+                act1.put(DATE_FIELD, dateStr);
+                act1.put(TIME_FIELD, timeStr);
+                act1.put(LOCATION_FIELD, locationStr);
+                act1.put(PLAYERS_FIELD,null); //this will be default, need to figure out inputting array
+                act1.put(RESULT_FIELD, null);
+
+                mDocRef.collection("act_objects").document("act1")
+                        .set(act1)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
+
                 //Maybe also add checks or change input type for time to be valid times (1-12 hrs, 0-59 min)??
 
                 //Go back to home screen after created
--- app/src/main/java/com/example/mariokarttournamentorganizer/EnterResultsActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/EnterResultsActivity.java
@@ -1,13 +1,22 @@
 package com.example.mariokarttournamentorganizer;
 
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class EnterResultsActivity extends AppCompatActivity {
@@ -19,6 +28,14 @@ public class EnterResultsActivity extends AppCompatActivity {
     private Button photo;
 
     private String resultsStr;
+
+    //for firebase
+    public static final String COMPLETED_FIELD= "completed";
+    public static final String RESULT_FIELD = "result";
+    public static final String TAG = "creatingACT";
+
+    private FirebaseFirestore mDocRef = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +51,28 @@ public class EnterResultsActivity extends AppCompatActivity {
             resultsStr = results.getText().toString();
             if (!resultsStr.isEmpty()) {
                 //TODO
+                //still need to actually get the right act  to edit
+
+                Map<String, Object> newInfo = new HashMap<>();
+                newInfo.put(COMPLETED_FIELD, true);
+                newInfo.put(RESULT_FIELD, resultsStr);
+
+
+
+                mDocRef.collection("act_objects").document("act1")
+                        .update(newInfo)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
             } else {
                 Toast.makeText(EnterResultsActivity.this,
                         "Please fill out necessary field.", Toast.LENGTH_SHORT).show();

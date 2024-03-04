7ea55d26b9cf7c1d0e1a3004cccbed1a7d6402b5
Matthew Berthoud <mwberthoud@wm.edu>
2024-03-04 13:01:20 -0500

    fetching and showing data, timestamp not formatted

--- app/src/main/java/com/example/mariokarttournamentorganizer/ViewACTActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/ViewACTActivity.java
@@ -1,5 +1,7 @@
 package com.example.mariokarttournamentorganizer;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 public class ViewACTActivity extends AppCompatActivity {
     private TextView whoTextView;
@@ -19,6 +34,12 @@ public class ViewACTActivity extends AppCompatActivity {
     private Button joinOrResults;
     private boolean admin = false; // HARDCODED, just for testing. Fetch based on auth from firebase
     private boolean complete = false; // HARDCODED, just for testing. Fetch based on ACT ID from firebase
+    private int ACTID = 7; // HARDCODED. Get from intent that launches page
+    // hardcoded the below to 7 as well
+    FirebaseFirestore db;
+    private DocumentReference docRef;
+//    private DocumentSnapshot actDocSnap;
+    private Map<String, Object> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +54,47 @@ public class ViewACTActivity extends AppCompatActivity {
         addToCalendar = (Button) findViewById(R.id.addToCalendarButton);
         joinOrResults = (Button) findViewById(R.id.joinOrResultsButton);
 
-        // check if ACT is complete or not based on boolean field in firebase
+        db = FirebaseFirestore.getInstance();
+        // hard code 7 for now
+        docRef = db.collection("act_objects").document("7");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        data = document.getData();
+                        Log.d(TAG, "DocumentSnapshot data: " + data);
+                        processData();
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
 
+    private void processData() {
+        if (data == null) {
+            Log.e(TAG, "invalid ACT id");
+            finish();
+        }
+
+        String playerString = "";
+        ArrayList players = (ArrayList) data.get("Players");
+        for (int i=0; i<8; i++) {
+            System.out.println(players.get(i));
+            playerString = playerString + (i+1) + ". " + players.get(i) + "\n";
+        }
+        whoTextView.setText(playerString);
+
+        whenTextView.setText(data.get("Date/Time").toString());
+        whereTextView.setText(data.get("location").toString());
+
+
+        // check if ACT is complete or not based on boolean field in firebase
         if (complete) {
             addToCalendar.setVisibility(View.GONE); // GONE = invisible and disabled
             joinOrResults.setVisibility(View.GONE);
@@ -46,7 +106,7 @@ public class ViewACTActivity extends AppCompatActivity {
         resultTextViewHeader.setVisibility(View.GONE);
         resultTextView.setVisibility(View.GONE);
 
-        addToCalendar.setOnClickListener(v-> addToCalendar());
+        addToCalendar.setOnClickListener(v -> addToCalendar());
 
         // check to see if the user is an admin for this ACT (if they created if or not)
         // for now, admin is hardcoded when assigned, for testing
@@ -55,7 +115,7 @@ public class ViewACTActivity extends AppCompatActivity {
         else // normal user perms, didn't create this particular ACT
             joinOrResults.setText("Join this ACT");
 
-        joinOrResults.setOnClickListener(v-> {
+        joinOrResults.setOnClickListener(v -> {
             if (admin) {
                 enterResults();
             } else {

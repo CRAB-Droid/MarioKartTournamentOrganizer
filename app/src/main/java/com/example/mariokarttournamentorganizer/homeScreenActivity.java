c9f047c85a1f4640d4c016ea4de237091e65b815
AJArnoldWM <ajarnold@email.wm.edu>
2024-03-04 03:42:46 -0500

    RecycleViews populate correctly based on value of "completed" in Firebase.
    The field in Firebase must be, character for character, "completed" for this to work.
    
    Implemented HashMap to control data correlated to ACTs.
    
    onStart function doesn't do anything (as far as I can tell) at the moment.

--- app/src/main/java/com/example/mariokarttournamentorganizer/homeScreenActivity.java
+++ app/src/main/java/com/example/mariokarttournamentorganizer/homeScreenActivity.java
@@ -29,10 +29,11 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class homeScreenActivity extends AppCompatActivity {
-
 
     RecyclerView upcomingRecyclerView;
     RecyclerView pastRecyclerView;
@@ -40,9 +41,9 @@ public class homeScreenActivity extends AppCompatActivity {
     TextView infoTextView;
     String [] upcomingArray = new String[10];
     public String [] pastArray = new String[10];
+
 //    ArrayList<String> upcomingArray = new ArrayList<>();
 //    ArrayList<String> pastArray = new ArrayList<>();
-
 
     public static final String ACTNAME_KEY = "name";
     public static final String DATE_KEY = "Date/Time";
@@ -50,12 +51,7 @@ public class homeScreenActivity extends AppCompatActivity {
     private CollectionReference collection = FirebaseFirestore.getInstance().collection("act_objects");
     //private CollectionReference collectionRef = dataBase.collection("act_objects");
 
-
-
-
-
-
-
+    //Not sure how to utilize this yet.
     @Override
     protected void onStart() {
         super.onStart();
@@ -71,8 +67,6 @@ public class homeScreenActivity extends AppCompatActivity {
                             return;
                         }
 
-
-
                         for (QueryDocumentSnapshot doc : value) {
                             //do something
                             Log.i("TAG", "Something updated");
@@ -80,13 +74,6 @@ public class homeScreenActivity extends AppCompatActivity {
                     }
                 });
     }
-
-
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,24 +85,124 @@ public class homeScreenActivity extends AppCompatActivity {
         pastRecyclerView = (RecyclerView) findViewById(R.id.pastRecyclerView);
         infoTextView = findViewById(R.id.infoTextView);
 
-        Intent currACT = new Intent(this, UserACTActivity.class);
-//        Intent pastACT = new Intent(this, adminActViewActivity.class);
+        fillACTS();
 
-        //Hardcoded values
-        for(int i = 0; i<10; i++){
-            upcomingArray[i] = "Upcoming Event " + i;
+          createACTButton.setOnClickListener(v->
+          {
+              Intent CreateACT = new Intent(this, CreateACTActivity.class);
+              startActivity(CreateACT);
+          });
+    }
+
+    public void firebaseDemo(View v) { // Delete when the demo is gone
+        Intent startDemo = new Intent(this, FirebaseDemoActivity.class);
+        startActivity(startDemo);
+    }
+
+    //Will iterate through all documents within the main ACT collection
+    //on Firebase, and fill in the recycleViews according to ACT
+    //information.
+    private void fillACTS(){
+
+        //Nested hashmap that will store the name of the ACT as the key,
+        //and another hashmap as the value.
+        //Reasoning for this:
+        //   - The getData() method returns a map.
+        //   - Need a way to differentiate between active/inactive ACTs.
+        //     This means we need the value associated with the "completed"
+        //     boolean field within the collection that is within the document.
+        //     To get this, we need the data within the document, which I am
+        //     using getData() for. You could easily just pull the specific
+        //     field, but why not pull everything for now
+        //     in case we want to display more info on recycleViews.
+        //   - We could use the information gained from the
+        //     ACT/ACT_Info hashmap to send over data through intents.
+        //     This is just spit-balling ideas though.
+
+        Map<String, Map<String,Object>> hashMap = new HashMap<>();
+
+            collection
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d("TAG", document.getId() + " => " + document.getData());
+
+                                //Add each document information to the hashmap.
+                                hashMap.put(document.getId().toString(), document.getData());
+                            }
+                            //This function MUST be after the for loop due to syncing
+                            //errors.
+                            setUpRecycleViews(hashMap);
+                        } else {
+                            Log.d("TagError", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void setUpRecycleViews(Map<String, Map<String,Object>> hashMap){
+
+        //Visualization of this nested loop:
+        //              ACT == {                                       }
+        //         ACT_Info ==            {                            }
+        //                      < ACT Name < Field , Value in field > >
+
+        for (Map.Entry<String, Map<String, Object>> ACT : hashMap.entrySet()) {
+            //Log.i("TRACKER", "Key: " + ACT.getKey() +". Value: " + ACT.getValue());
+            Map<String, Object> innerMap = ACT.getValue();
+            for (Map.Entry<String, Object> ACT_Info : innerMap.entrySet()) {
+                //Log.i("TRACKER", "Key: " + ACT_Info.getKey() +". Value: " + ACT_Info.getValue());
+                if(ACT_Info.getKey().equals("completed") && ACT_Info.getValue() instanceof Boolean){
+                    //Log.i("HIT", ACT_Info.getValue().toString());
+                    boolean completed = (boolean) ACT_Info.getValue();
+                    if(completed){
+                        //Log.i("HIT_TRUE", ACT_Info.getValue().toString());
+                        for(int i = 0; i < pastArray.length; i++){
+                            if(pastArray[i] == null){
+                                pastArray[i] = ACT.getKey().toString();
+                                break;
+                            }
+                        }
+                    }
+                    else {
+                        //Log.i("HIT_FALSE", ACT_Info.getValue().toString());
+                        for(int i = 0; i < upcomingArray.length; i++){
+                            if(upcomingArray[i] == null){
+                                upcomingArray[i] = ACT.getKey().toString();
+                                break;
+                            }
+                        }
+                    }
+                } else if (ACT_Info.getKey() == "completed") {
+                    Log.i("Incorrect Declaration: boolean",
+                            "'Completed' field not filled out correctly in Firebase.");
+                }
+
+                //System.out.println("Inner Key: " + innerKey + ", Inner Value: " + innerValue);
+            }
         }
+        Intent pastACT = new Intent(this, adminActViewActivity.class);
+        Intent currACT = new Intent(this, UserACTActivity.class);
 
-
-        fillPastACTS();
-
-        //Log.i("Tag", pastArray[0]);
-
-
-        //Set up recycle view for the upcoming ACT events.//
+        RecyclerViewAdapter pastCustomAdapter = new RecyclerViewAdapter(pastArray);
         RecyclerViewAdapter upcomingCustomAdapter = new RecyclerViewAdapter(upcomingArray);
+
+        pastRecyclerView.setAdapter(pastCustomAdapter);
         upcomingRecyclerView.setAdapter(upcomingCustomAdapter);
+
+        pastRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         upcomingRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemClickSupport.addTo(pastRecyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                infoTextView.setText(pastArray[position]);
+                startActivity(pastACT);
+            }
+        });
 
         //Handle the clicking of certain rows, where position is the item clicked.
         ItemClickSupport.addTo(upcomingRecyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
@@ -125,77 +212,5 @@ public class homeScreenActivity extends AppCompatActivity {
                 startActivity(currACT);
             }
         });
-
-        //Set up recycle view for the past ACT events.//////////////
-//        RecyclerViewAdapter pastCustomAdapter = new RecyclerViewAdapter(pastArray);
-//        pastRecyclerView.setAdapter(pastCustomAdapter);
-//        pastRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        ItemClickSupport.addTo(pastRecyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
-//            @Override
-//            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
-//                infoTextView.setText(pastArray[position]);
-//                startActivity(pastACT);
-//            }
-//        });
-
-          createACTButton.setOnClickListener(v->
-          {
-              Intent CreateACT = new Intent(this, CreateACTActivity.class);
-              startActivity(CreateACT);
-          });
     }
-
-
-    public void firebaseDemo(View v) { // Delete when the demo is gone
-        Intent startDemo = new Intent(this, FirebaseDemoActivity.class);
-        startActivity(startDemo);
-    }
-
-    private void fillPastACTS(){
-//        Log.i("Hereeeeeeee", "checkpoint 1");
-//
-            collection
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        ///Log.i("Hereeeeeeee", "checkpoint 2");
-                        if (task.isSuccessful()) {
-//                            int size = task.getResult().size();
-//                            pastArray = new String[size];
-                            int i = 0;
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                //Log.d(TAG, document.getId() + " => " + document.getData());
-                                pastArray[i] = document.getId().toString();// + ", " + document.getDate(DATE_KEY);
-                                //Log.i("Tag", pastArray[i]);
-                                i++;
-                                //Log.i("Tag", document.getId().toString());
-                            }
-                            setUpPastRecycleView();
-                        } else {
-                            Log.d("TagError", "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-        //Log.i("Tag1", pastArray[0]);
-    }
-
-    private void setUpPastRecycleView(){
-
-        Intent pastACT = new Intent(this, adminActViewActivity.class);
-
-        RecyclerViewAdapter pastCustomAdapter = new RecyclerViewAdapter(pastArray);
-        pastRecyclerView.setAdapter(pastCustomAdapter);
-        pastRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        ItemClickSupport.addTo(pastRecyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
-            @Override
-            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
-                infoTextView.setText(pastArray[position]);
-                startActivity(pastACT);
-            }
-        });
-    }
-
 }
